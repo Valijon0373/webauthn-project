96d5d4f4b378aa33ecbd4da8b694ae647ecd57b5
Valijon <d.valijon2001@gmail.com>
2026-04-06 11:59:26 +0500

    Add webauthn credentials to users in JSON data

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
+    allowedHosts: ['puppylike-subadministratively-kate.ngrok-free.dev'],
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
